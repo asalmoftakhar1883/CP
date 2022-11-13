7b583c26b03ba42c68829a47699e57ff647b893a
Asal Moftakhar <117561657+asalmoftakhar1883@users.noreply.github.com>
2022-11-13 22:25:28 +0330

    Add files via upload

---
+++ main 4.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+   using namespace std;
+
+    int main() {
+
+     char English;
+     
+     cout<<" Please enter an english word: ";
+     cin>> English;
+     switch (English) {
+     	case 'a' :
+     		cout<<"VOWEL";
+     		break;
+     		
+     	case 'e' :
+     		cout<<"VOWEL";
+     		break;
+     		
+     		
+     	case 'i' :
+     		cout<<"VOWEL";
+     		break;
+     			
+     	case 'o' :
+     		cout<<"VOWEL";
+     		break;
+     		
+     	case 'u' :
+     		cout<<"VOWEL";
+     		break;
+     			
+	 }
+
+
+}
